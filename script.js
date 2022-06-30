1f4488600ab3cc1b283840bea808576699f068a3
YaaseenIbrahim <yaseenibrahimm12@gmail.com>
2022-06-30 15:04:33 +0500

    Added counters for wins, losses and draws

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const userChoiceDisplay = document.querySelector('#userChoiceDisplay')
 const resultText = document.querySelector('#resultText')
 const buttons = Array.from(document.querySelectorAll('button'))
 const options = ["Rock", "Paper", "Scissors"]
-
+const winsDisplay = document.querySelector('#counters').children[0]
+const lossesDisplay = document.querySelector('#counters').children[1]
+const drawsDisplay = document.querySelector('#counters').children[2]
+let draws = wins = losses = 0
 
 buttons.forEach(button => {
     button.onclick = () => {
@@ -15,18 +18,28 @@ buttons.forEach(button => {
         
         if (userChoiceDisplay.innerText == compChoiceDisplay.innerText) {
             resultText.innerText = "It's a draw!"
+            draws = draws+1
+            drawsDisplay.innerText = 'Draws:'+ draws 
         } 
         else if (userChoiceDisplay.innerText == options[compChoice+1]) {
             resultText.innerText = "You won!"
+            wins = wins + 1
+            winsDisplay.innerText = 'Wins:'+ wins
         } 
         else if (userChoiceDisplay.innerText == options[compChoice-1]){
             resultText.innerHTML = "You lost!"
+            losses = losses + 1
+            lossesDisplay.innerText = 'Losses:' + losses
         }
         else if (compChoice == 0 ){ //For the comp=rock and you=scissor combo since array cant -1 above limit
             resultText.innerText = "You lost!"
+            losses = losses + 1
+            lossesDisplay.innerText = 'Losses:' + losses
         }
         else { //For the you=rock and comp=scissor combo since array cant +1 above limit
             resultText.innerText = "You won!"
+            wins = wins + 1
+            winsDisplay.innerText = 'Wins:' + wins
         }
     }
 })
